e1ebdea2e034baef5f0cd7b7df5d378de77662dd
Samuca <samuelmadeiradev@gmail.com>
2024-03-27 15:11:30 -0300

    Adicionado model e controller para turma

---
+++ db/migrate/20240327175630_add_aluno_id_to_turmas.rb
@@ -0,0 +1,4 @@
+class AddAlunoIdToTurmas < ActiveRecord::Migration[7.1]
+  def change
+  end
+end
---
+++ db/migrate/20240327175707_add_professor_id_to_turmas.rb
@@ -0,0 +1,4 @@
+class AddProfessorIdToTurmas < ActiveRecord::Migration[7.1]
+  def change
+  end
+end
