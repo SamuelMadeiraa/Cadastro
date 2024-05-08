4d60bf3dbcbecea73817ef096923eb8d9e683d06
Samuca <samuelmadeiradev@gmail.com>
2024-05-08 16:01:16 -0300

    Criando uma nova Branche para testar a aplicação

--- spec/models/aluno_spec.rb
+++ spec/models/aluno_spec.rb
@@ -1,12 +1,1 @@
 require 'rails_helper'
-
-RSpec.describe Aluno, type: :model do
-  it 'Validação inicial ' do 
-    expect('hello').to eq('hello')
-    end
-
-    it 'validação que vai dar erro ' do 
-      hell = "batata"
-      expect(hell).not_to eql('hello')
-      end
-end
